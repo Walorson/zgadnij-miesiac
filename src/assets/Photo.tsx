7e0c6cef98704a0c07f606393c9b81595ebef035
Walorson <zembakii@gmail.com>
2026-03-19 22:42:21 +0100

    Drop nowych fotek

--- src/assets/Photo.tsx
+++ src/assets/Photo.tsx
@@ -34,7 +34,6 @@ export const Photo = (props: any) => {
         <div className="photo">
             <div id="photo">
                 <img src={"./img/"+props.photo.url} />
-                <div id="photo-year">{props.photo.year}</div>
             </div>
             <input type="range" min="1"  max="12" id="month-input" defaultValue="6" onInput={(e) => updateMonthName(e)} disabled={endRound ? true : false}/>
             <div id="month-name">Czerwiec</div>
